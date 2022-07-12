00dcd13500da1c749640b8c5271881f4487e921c
kushandesilva59 <kushandesilva801@gmail.com>
2022-07-12 12:24:46 +0530

    updated

--- src/Controller/SearchStudentController.java
+++ src/Controller/SearchStudentController.java
@@ -1,4 +1,22 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class SearchStudentController {
+    public AnchorPane searchContext;
+    public TextField txtSearch;
+    public TextField txtNic;
+    public TextField txtEmail;
+    public TextField txtName;
+    public TextField txtId;
+    public TextField txtContact;
+    public TextField txtAddress;
+
+    public void backOnaction(ActionEvent event) {
+    }
+
+    public void searcOnAction(ActionEvent event) {
+    }
 }
