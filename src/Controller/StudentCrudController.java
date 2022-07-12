eda47d2bd089c7557f6e49ae329f82a2eeeb7191
kushandesilva59 <kushandesilva801@gmail.com>
2022-07-12 10:56:46 +0530

    create generate new id method

--- src/Controller/StudentCrudController.java
+++ src/Controller/StudentCrudController.java
@@ -3,8 +3,10 @@ package Controller;
 import Model.Student;
 import Util.CrudUtil;
 
+import javax.management.Query;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 public class StudentCrudController {
     public Student searchStudent(String studentId) throws SQLException, ClassNotFoundException {
@@ -20,5 +22,35 @@ public class StudentCrudController {
             student.setNic(resultSet.getString(6));
         }
         return student;
+    }
+
+    public String generateNewId() throws SQLException, ClassNotFoundException {
+        ResultSet resultSet = CrudUtil.executeQuery("SELECT student_id FROM student ORDER BY student_id DESC");
+        
+
+        String newUserId = "";
+        int integer = 0;
+
+        String lastUserId = resultSet.getString(1);
+        String[] split = lastUserId.split("[A-z]");
+        if(split.length>0){
+            integer = Integer.valueOf(split[2]);
+            ++integer;
+        }
+
+
+        if(resultSet.next()){
+            if (integer>=100) {
+                newUserId = "S00-" + String.valueOf(integer) ;
+            }else if(integer>=10){
+                newUserId = "S00-0" + String.valueOf(integer);
+            }else{
+                newUserId = "S00-00" + String.valueOf(integer);
+            }
+            return newUserId;
+
+        }else{
+            return "S00-001";
+        }
     }
 }
