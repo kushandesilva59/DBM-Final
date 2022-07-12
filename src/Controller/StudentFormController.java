b2d50dc1838c5053e5b8bc449053161134958e55
kushandesilva59 <kushandesilva801@gmail.com>
2022-07-12 10:35:16 +0530

    create student form ui and implement methods

--- src/Controller/StudentFormController.java
+++ src/Controller/StudentFormController.java
@@ -1,4 +1,34 @@
 package Controller;
 
+import Model.Student;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class StudentFormController {
+    public AnchorPane studentFormContext;
+    public TableView<Student> tblStudents;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colMail;
+    public TableColumn colContact;
+    public TableColumn colAddress;
+    public TableColumn colNIC;
+    public Button btnSave;
+    public TextField txtName;
+    public TextField txtEmail;
+    public TextField txtContact;
+    public TextField txtAddress;
+    public TextField txtNic;
+
+    public void saveOnAction(ActionEvent event) {
+        
+    }
+
+    public void deleteOnAction(ActionEvent event) {
+
+    }
 }
--- src/Util/DBConnection.java
+++ src/Util/DBConnection.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import static java.lang.Class.forName;
 
 public class DBConnection {
     private static DBConnection dbConnection;
